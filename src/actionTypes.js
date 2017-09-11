f7fe725969e1dfe44aa62994ba09534b0678d703
Mahmoud <naggar218@gmail.com>
2017-09-12 01:43:05 +0200

    finished - now have better understanding of what ImmutableJS is

---
+++ src/actionTypes.js
@@ -0,0 +1,8 @@
+export default {
+  reviews: {
+    ADD_REVIEW: "ADD_REVIEW",
+    DELETE_REVIEW: "DELETE_REVIEW",
+    FLAG_REVIEW: "FLAG_REVIEW",
+    RATE_REVIEW: "RATE_REVIEW"
+  }
+};
---
+++ src/reducers/reviews.js
@@ -0,0 +1,40 @@
+import { List, Map } from "immutable";
+import types from "../actionTypes";
+
+const reviews = (state = List(), action) => {
+  switch (action.type) {
+    case types.reviews.ADD_REVIEW:
+      const newReview = Map({
+        id: action.id,
+        item_id: action.item_id,
+        reviewer: action.reviewer,
+        rating: action.rating,
+        flag: false
+      });
+      return state.push(newReview);
+
+    case types.reviews.DELETE_REVIEW:
+      return state.filter(review => review.id !== action.id);
+
+    case types.reviews.FLAG_REVIEW:
+      return state.map(
+        review =>
+          review.id === action.id
+            ? Object.assign({}, review, { flag: action.flag })
+            : review
+      );
+
+    case types.reviews.RATE_REVIEW:
+      return state.map(
+        review =>
+          review.id === action.id
+            ? { ...review, rating: action.rating }
+            : review
+      );
+
+    default:
+      return state;
+  }
+};
+
+export default reviews;
